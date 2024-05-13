884d909a16c2abcb31fdf9f29fe8d56aa0924cd5
SandroGoD9 <106669841+SandroGoD9@users.noreply.github.com>
2024-05-13 09:10:10 -0300

    Add files via upload

---
+++ projeto1_sandrogod9-main/lista.js
@@ -0,0 +1,4 @@
+document.querySelectorAll("#lista-itens").forEach(function(itens)
+{item.addEventListener('click', function();
+    document.getElementById ('mensagem-item'), text.content = this.getAttribute("data-mensagem");)
+});
